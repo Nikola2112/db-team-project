b8520c5cedda14e2c716997841ce8fbf15633405
julioz31 <4projectsanduni@gmail.com>
2025-06-08 17:18:36 +0200

    Add files via upload

---
+++ docs/software/SpringSoftApp.java
@@ -0,0 +1,14 @@
+package com.example.software.;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoftwareApplication.class, args);
+	}
+
+}
+
