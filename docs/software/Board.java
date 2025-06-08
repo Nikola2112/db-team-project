745061fe6b8105adf67b10cf2f2def9cc19a55fb
julioz31 <4projectsanduni@gmail.com>
2025-06-08 17:35:54 +0200

    Create Board.java

---
+++ docs/software/Board.java
@@ -0,0 +1,22 @@
+package com.example.software.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+}
